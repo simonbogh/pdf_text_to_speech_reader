8df861b54cf521a6791df3df95e54c62bc7e9d56
Simon Bøgh <sb@m-tech.aau.dk>
2023-01-28 17:39:08 +0100

    Initial commit of code

---
+++ pdf_text2speech.py
@@ -0,0 +1,30 @@
+import PyPDF2
+import pyttsx3
+
+# Open the PDF file
+pdfFileObj = open('./document.pdf', 'rb')
+
+# Create a PDF reader object
+pdfReader = PyPDF2.PdfReader(pdfFileObj)
+
+# Initialize the text-to-speech engine
+engine = pyttsx3.init()
+
+# Set voice to Samantha (macOS)
+# engine.setProperty('voice', 'com.apple.speech.synthesis.voice.samantha')
+
+# Set the rate of the voice
+engine.setProperty('rate', 190)
+
+# Iterate through each page of the PDF
+for _, page in enumerate(pdfReader.pages):
+    # Extract the text from the page
+    text = page.extract_text()
+    # Print the text
+    print(text)
+    # Speak the text
+    engine.say(text)
+    engine.runAndWait()
+
+# Close the PDF file
+pdfFileObj.close()
